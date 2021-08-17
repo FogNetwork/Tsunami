fda20fad95ae4dcc913585fae97d5cf3f679cd71
Pers0n <81875430+th3pers0n@users.noreply.github.com>
2021-08-17 10:12:28 -0700

    Create index

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express')
+const app = express()
+const config = require('./config.json')
+const port = config.port
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at localhost:${port}`)
+})
