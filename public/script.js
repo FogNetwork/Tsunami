8421745ee326a030df8bc96a404f97dd04e248bd
Pers0n <81875430+th3pers0n@users.noreply.github.com>
2021-08-19 12:50:02 -0700

    Proxy script

--- public/script.js
+++ public/script.js
@@ -1,1 +1,27 @@
+search = document.getElementById('search');
+search.addEventListener('keyup', function onEvent(e) {
+    if (e.keyCode === 13) {
+        go(search.value)
+    }
+});
 
+
+function go(url) {
+if (url !== '') {
+if (url.includes('.')) {
+alert(url)
+} else if (url.startsWith('https://')) {
+open(url)
+} else if (url.startsWith('http://')) {
+open(url)
+} else {
+open('https://www.google.com/search?q=' + url)
+}
+} else {
+return false;
+}
+}
+
+function open (url) {
+alert(url)
+}
