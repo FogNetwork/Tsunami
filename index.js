fb8aef0350d04afb5a04421499527309f050001f
Pers0n <81875430+th3pers0n@users.noreply.github.com>
2021-08-19 13:14:47 -0700

    express not working

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const config = require('./config.json')
-const port = config.port
+const port = '8080'
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
