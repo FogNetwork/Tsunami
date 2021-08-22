6d0135db79436dde132315af9b88fbebc31c9d66
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2021-08-22 14:51:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const config = require('./config.json')
 const port = config.port
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
