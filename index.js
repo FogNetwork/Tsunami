7140478284dbc9e065c04e13d6465ec4425b7763
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2021-08-22 15:29:06 -0700

    not working...

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const app = express()
-const path = require('path')
-const http = require('http').Server(app);
 const config = require('./config.json')
 const port = config.port
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+}); 
 
 app.listen(port, () => {
   console.log(`Server running at localhost:${port}`)
