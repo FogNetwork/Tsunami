f72396c197150f4047789b110030dc2c19321cb2
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2021-08-24 18:01:44 -0700

    404

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const port = process.env.PORT || config.port
 
 app.use(express.static('./public', {
     extensions: ['html', 'htm'],
+    res.status(404).send('404');
 }));
 
 app.get('/', function(req, res){
     res.sendFile('index.html', {root: './public'});
 }); 
-
-app.get('*', function(req, res){
-    res.status(404).render('404.html', {root: './public'});
-});
 
 app.listen(port, () => {
   console.log(`Server running at localhost:${port}`)
