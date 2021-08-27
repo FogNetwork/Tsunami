edc9b78c7976400f74a6dd87a13bdeb1ab571e8a
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2021-08-27 16:14:09 -0700

    later

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const corrosion = require('corrosion'),
 const config = require('./config.json')
 const port = process.env.PORT || config.port
 
@@ -16,20 +15,6 @@ app.listen(port, () => {
   console.log(`Server running at localhost:${port}`)
 })
 
-
-const proxy = new corrosion({
-    prefix: '/corrosion/',
-    title: 'Tsunami',
-    ws: true,
-    codec: 'xor',
-    requestMiddleware: [
-        corrosion.middleware.blacklist([
-            'accounts.google.com',
-        ], 'Page is blocked'),
-    ],
-});
-
 app.use(function (req, res) {
-        if (req.url.startsWith("/corrosion/")) return proxy.request(req, res);
     res.status(404).sendFile('404.html', {root: './public'});
 });
