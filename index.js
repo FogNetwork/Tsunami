8fb1fb6df22a7e4b4d85d8b66be024fd70786024
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2021-08-30 20:40:49 -0700

    cool thing

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/*
+  ______            _   _      _                      _    
+ |  ____|          | \ | |    | |                    | |   
+ | |__ ___   __ _  |  \| | ___| |___      _____  _ __| | __
+ |  __/ _ \ / _` | | . ` |/ _ \ __\ \ /\ / / _ \| '__| |/ /
+ | | | (_) | (_| | | |\  |  __/ |_ \ V  V / (_) | |  |   < 
+ |_|  \___/ \__, | |_| \_|\___|\__| \_/\_/ \___/|_|  |_|\_\
+             __/ |                                         
+            |___/    
+Made by Nebelung
+MIT license: http://opensource.org/licenses/MIT
+*/
+
 const express = require('express')
 const app = express()
 const config = require('./config.json')
