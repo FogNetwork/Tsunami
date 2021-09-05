62aa7ccacac069460f841a5fb8a5e59b06fe7d1c
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2021-09-05 15:31:12 -0700

    search games

--- public/js/game.js
+++ public/js/game.js
@@ -1,1 +1,13 @@
+function searchgames() {
+  var searchgames = document.getElementById("searchgames");
+  var filter = search.value.toLowerCase();
+  var game = document.getElementsByClassName('game');
 
+  for (i = 0; i < game.length; i++) {
+    if (game[i].innerText.toLowerCase().includes(filter)) {
+      game[i].style.display = "initial";
+    } else {
+      game[i].style.display = "none";
+    }
+  }
+}
