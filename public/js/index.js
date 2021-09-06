3f2d1ed81fc6049b26550130ac26747d485daac4
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2021-09-06 11:39:46 -0700

    tab cloak

--- public/js/index.js
+++ public/js/index.js
@@ -4,3 +4,22 @@
 // |  _| (_) | (_| | | |\  |  __/ |_ \ V  V / (_) | |  |   < 
 // |_|  \___/ \__, | |_| \_|\___|\__| \_/\_/ \___/|_|  |_|\_\
 //            |___/                                          
+
+var title = localStorage.getItem("title")
+var favicon = localStorage.getItem("favicon")
+
+if (localStorage.hasOwnProperty("title")) {
+    document.title = title
+}
+
+if (localStorage.hasOwnProperty("favicon")) {
+    document.querySelector("link[rel='shortcut icon']").href = favicon;
+}
+
+function settitle(title) {
+  localStorage.setItem("title", title)
+}
+
+function setfavicon(icon) {
+  localStorage.setItem("favicon", icon)
+}
