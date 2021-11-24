785d99de08f52a87138ad6114a7a18054e61edb7
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2021-11-24 01:41:37 +0000

    secret feature

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const username = config.username
 const password = config.password
 const users = {}
 users[username] = password
+const ytdl = require("ytdl-core");
 
 const proxy = new Corrosion({
     prefix: "/corrosion/",
@@ -25,8 +26,8 @@ const proxy = new Corrosion({
     forceHttps: true,
     requestMiddleware: [
         Corrosion.middleware.blacklist([
-            'accounts.google.com',
-        ], 'Page is blocked'),
+            "accounts.google.com",
+        ], "Page is blocked"),
     ]
 });
 
@@ -51,28 +52,41 @@ app.use(basicAuth({
 
 function autherror(req) {
     return req.auth
-        ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')
-        : 'Error'
+        ? ("Credentials " + req.auth.user + ":" + req.auth.password + " rejected")
+        : "Error"
 }
 
 if (lite == "true") {
 
-app.get('/', function(req, res){
-    res.sendFile('index.html', {root: './lite'});
+app.get("/", function(req, res){
+    res.sendFile("index.html", {root: "./lite"});
 });
 
-app.get('/js/go.js', function(req, res){
-    res.sendFile('go.js', {root: './lite'});
+app.get("/js/go.js", function(req, res){
+    res.sendFile("go.js", {root: "./lite"});
 });
 
 }
 
-app.use(express.static('./public', {
-    extensions: ['html']
+app.use(express.static("./public", {
+    extensions: ["html"]
 }));
 
-app.get('/', function(req, res){
-    res.sendFile('index.html', {root: './public'});
+app.get("/", function(req, res){
+    res.sendFile("index.html", {root: "./public"});
+});
+
+app.get("/watch", (req, res) => {
+  const url = "https://www.youtube.com/watch?v=" + req.query.v
+  if (req.query.audio !== "true") {
+  ytdl(url).on("response", response => {
+  res.setHeader("content-length", response.headers["content-length"])
+  }).pipe(res)
+  } else {
+  ytdl(url, {filter: "audioonly"}).on("response", response => {
+  res.setHeader("content-length", response.headers["content-length"])
+  }).pipe(res)
+  }
 });
 
 app.use(function (req, res) {
@@ -81,7 +95,7 @@ app.use(function (req, res) {
     } else if (req.url.startsWith(palladium.prefix)) {
       return palladium.request(req, res)
     } else {
-      res.status(404).sendFile('404.html', {root: './public'});
+      res.status(404).sendFile("404.html", {root: "./public"});
     }
 })
 
