0a640783f4dcf4f6a7afa3581b3aaa08cd4bb31d
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2021-11-25 00:52:09 +0000

    fixes

--- public/js/youtube.js
+++ public/js/youtube.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function() {
 searchyoutube = document.getElementById("searchyoutube");
 searchyoutube.addEventListener('keyup', function onEvent(e) {
     if (e.keyCode === 13) {
-        window.location.href = "/watch?v=" + searchyoutube.value.split("https://www.youtube.com/watch?v=")
+        window.location.href = "/watch?v=" + searchyoutube.value.split("https://www.youtube.com/watch?v=")[1]
     }
 });
 })
